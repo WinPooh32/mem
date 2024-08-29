64f63cf5d86c4556c1ec2821716c9fafac71e9f1
WinPooh32 <winpooh32@yandex.ru>
2024-08-30 02:47:24 +1000

    Refactor test

--- merge_test.go
+++ merge_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func iterate[K comparable, V any](ks []K, vs []V) iter.Seq2[K, V] {
@@ -23,73 +22,73 @@ func iterate[K comparable, V any](ks []K, vs []V) iter.Seq2[K, V] {
 
 func TestMerge_2(t *testing.T) {
 	type args struct {
-		a iter.Seq2[string, int]
-		b iter.Seq2[string, int]
+		seqs []iter.Seq2[string, int]
 	}
 	tests := []struct {
-		name string
-		args args
-		want iter.Seq2[string, int]
+		name     string
+		args     args
+		wantKeys []string
+		wantVals []int
 	}{
 		{
 			name: "a and b lengths are equal",
-			args: args{
-				a: iterate([]string{"a", "b", "c"}, []int{1, 2, 3}),
-				b: iterate([]string{"a", "b", "c"}, []int{1, 2, 3}),
-			},
-			want: iterate([]string{"a", "a", "b", "b", "c", "c"}, []int{1, 1, 2, 2, 3, 3}),
+			args: args{[]iter.Seq2[string, int]{
+				iterate([]string{"a", "b", "c"}, []int{1, 2, 3}),
+				iterate([]string{"a", "b", "c"}, []int{1, 2, 3}),
+			}},
+			wantKeys: []string{"a", "a", "b", "b", "c", "c"},
+			wantVals: []int{1, 1, 2, 2, 3, 3},
 		},
 		{
 			name: "a shorter than b",
-			args: args{
-				a: iterate([]string{"a"}, []int{1}),
-				b: iterate([]string{"b", "c"}, []int{2, 3}),
-			},
-			want: iterate([]string{"a", "b", "c"}, []int{1, 2, 3}),
+			args: args{[]iter.Seq2[string, int]{
+				iterate([]string{"a"}, []int{1}),
+				iterate([]string{"b", "c"}, []int{2, 3}),
+			}},
+			wantKeys: []string{"a", "b", "c"},
+			wantVals: []int{1, 2, 3},
 		},
 		{
 			name: "b shorter than a",
-			args: args{
-				a: iterate([]string{"b", "c"}, []int{2, 3}),
-				b: iterate([]string{"a"}, []int{1}),
-			},
-			want: iterate([]string{"a", "b", "c"}, []int{1, 2, 3}),
+			args: args{[]iter.Seq2[string, int]{
+				iterate([]string{"b", "c"}, []int{2, 3}),
+				iterate([]string{"a"}, []int{1}),
+			}},
+			wantKeys: []string{"a", "b", "c"},
+			wantVals: []int{1, 2, 3},
 		},
 		{
 			name: "a is empty",
-			args: args{
-				a: iterate([]string{}, []int{}),
-				b: iterate([]string{"b", "c"}, []int{2, 3}),
-			},
-			want: iterate([]string{"b", "c"}, []int{2, 3}),
+			args: args{[]iter.Seq2[string, int]{
+				iterate([]string{}, []int{}),
+				iterate([]string{"b", "c"}, []int{2, 3}),
+			}},
+			wantKeys: []string{"b", "c"},
+			wantVals: []int{2, 3},
 		},
 		{
 			name: "b is empty",
-			args: args{
-				a: iterate([]string{"a"}, []int{1}),
-				b: iterate([]string{}, []int{}),
-			},
-			want: iterate([]string{"a"}, []int{1}),
+			args: args{[]iter.Seq2[string, int]{
+				iterate([]string{"a"}, []int{1}),
+				iterate([]string{}, []int{}),
+			}},
+			wantKeys: []string{"a"},
+			wantVals: []int{1},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			nextWant, stopWant := iter.Pull2(tt.want)
-			defer stopWant()
-
 			gotKeys := []string{}
+			gotVals := []int{}
 
-			for k, v := range Merge(tt.args.a, tt.args.b) {
-				wantK, wantV, ok := nextWant()
-				require.True(t, ok)
-
-				assert.Equal(t, wantK, k)
-				assert.Equal(t, wantV, v)
-
+			for k, v := range Merge(tt.args.seqs[0], tt.args.seqs[1], tt.args.seqs[2:]...) {
 				gotKeys = append(gotKeys, k)
+				gotVals = append(gotVals, v)
 			}
 
-			assert.IsNonDecreasing(t, gotKeys)
+			if assert.Equal(t, tt.wantKeys, gotKeys) && assert.Equal(t, tt.wantVals, gotVals) {
+				assert.IsNonDecreasing(t, gotKeys)
+			}
 		})
 	}
 }
